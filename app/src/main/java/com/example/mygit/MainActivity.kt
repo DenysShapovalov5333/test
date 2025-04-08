4c1b48648a5a4d859a7533e0477f7e5e06ae1fb1
d.shapovalov.tpa <d.shapovalov.tpa@pivdenny.ua>
2025-04-08 16:38:42 +0300

    new adres

--- app/src/main/java/com/example/mygit/MainActivity.kt
+++ app/src/main/java/com/example/mygit/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mygit
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+        Log.d("TAG", "onCreate:  я переключився на адрес")
     }
 }
